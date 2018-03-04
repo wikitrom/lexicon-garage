31236d3efe0ad79e843b01fe975ec7bcd4c1c81e
Mats Bergstroem <hackingtrom@gmail.com>
2018-03-04 15:56:07 +0100

    Fix bugs in Garage class
    - Update constructor
    - Update addVehicle
    - Add alternative setParkingStatus method

--- src/se/lexicon/em/model/Garage.java
+++ src/se/lexicon/em/model/Garage.java
@@ -8,41 +8,36 @@ import se.lexicon.em.exceptions.VehicleNotFoundException;
 import se.lexicon.em.utilities.ParkingStatusType;
 import se.lexicon.em.utilities.VehicleType;
 
-public class Garage
-{
+public class Garage {
 	private String location;
 	private int maxNoOfParkedVehicles;
-	private int currentNoOfParkedVehicles;
-	private Vehicle[] vehicleArray = new Vehicle[100];
+	private int currentNoOfParkedVehicles = 0;
+	private Vehicle[] vehicleArray;
 	private int nextPosition = 0;
 	private int noOfRegisteredVehicles = 0;
 	ArrayList<Vehicle> vehicles = new ArrayList<Vehicle>();
 	Vehicle pVehicle;
 
-	public Garage(String location, int maxNoOfParkedVehicles)
-	{
+	public Garage(String location, int maxNoOfParkedVehicles) {
 		this.maxNoOfParkedVehicles = maxNoOfParkedVehicles;
+		vehicleArray = new Vehicle[maxNoOfParkedVehicles];
 	}
 
-	public Vehicle[] getVehicleArray()
-	{
+	public Vehicle[] getVehicleArray() {
 		return vehicleArray;
 	}
 
-	public void addVehicle(Vehicle newVehicle)
-	{
+	public void addVehicle(Vehicle newVehicle) {
 		vehicleArray[nextPosition] = newVehicle;
 		nextPosition++;
-		// vehicles.addAll(vehicles);
+		currentNoOfParkedVehicles++;
+		noOfRegisteredVehicles++;
 
 	}
 
-	public Vehicle findVehicles(int regNumber) throws VehicleNotFoundException
-	{
-		for (int count = 0; count < vehicleArray.length; count++)
-		{
-			if (vehicleArray[count] != null)
-			{
+	public Vehicle findVehicles(int regNumber) throws VehicleNotFoundException {
+		for (int count = 0; count < vehicleArray.length; count++) {
+			if (vehicleArray[count] != null) {
 				if (vehicleArray[count].getRegNumber() == regNumber)
 					return vehicleArray[count];
 			}
@@ -52,52 +47,51 @@ public class Garage
 
 	}
 
-	public boolean isGarageFull()
-	{
+	public boolean isGarageFull() {
 		return (currentNoOfParkedVehicles == maxNoOfParkedVehicles) ? true : false;
 
 	}
 
-	public boolean canTackeMoreCustomer()
-	{
+	public boolean canTackeMoreCustomer() {
 		return (noOfRegisteredVehicles < vehicleArray.length) ? true : false;
 
 	}
 
-	public void unparkVehicle(int regNumber)
-	{
+	public void unparkVehicle(int regNumber) {
 		Vehicle aVehicle;
-		try
-		{
+		try {
 			aVehicle = findVehicles(regNumber);
-			if (aVehicle != null)
-			{
+			if (aVehicle != null) {
 				aVehicle.setStatus(ParkingStatusType.NOT_PARKED);
 				currentNoOfParkedVehicles--;
 			}
 
-		}
-		catch (VehicleNotFoundException e)
-		{
+		} catch (VehicleNotFoundException e) {
 
 		}
 
 		// no need to do anything
 	}
 
-	public void setParkingStatus(ParkingStatusType status, int regNumbr)
-	{
+	public void setParkingStatus(ParkingStatusType status, int regNumbr) {
 		{
+			// FIX: MATS - this will not work, use next method instead
 
-			if (pVehicle.getRegNumber() != regNumbr)
-			{
+			if (pVehicle.getRegNumber() != regNumbr) {
 				pVehicle.setStatus(status.NOT_PARKED);
 			}
-			if (pVehicle.getRegNumber() == regNumbr)
-			{
+			if (pVehicle.getRegNumber() == regNumbr) {
 				pVehicle.setStatus(status.PARKED);
 			}
 
+		}
+	}
+
+
+	public void setParkingStatus(Vehicle aVehicle, ParkingStatusType status) {
+
+		if (aVehicle != null) {
+			aVehicle.setStatus(status);
 		}
 	}
 
